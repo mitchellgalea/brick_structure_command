e44b1066b740fe7f60e63536c412676804045a17
mitchellgalea <48578916+mitchellgalea@users.noreply.github.com>
2019-03-20 21:08:03 +1100

    Using templates

--- include/brick_structure_command/brick.h
+++ include/brick_structure_command/brick.h
@@ -1,16 +1,49 @@
 #ifndef BRICK_H
 #define  BRICK_H
 
+template<struct BrickType>
 class Brick {
-private:
-    double x_, y_, z_;
 public:
-    Brick();
+    Brick()
+    double getXDim() const;
+    double getYDim() const;
+    double getZDim() const;
+};
 
-    double getX() const;
-    double getY() const;
-    double getZ() const;
+struct RedBrick {
+    static const double x_dim;
+    static const double y_dim;
+    static const double z_dim;
+    static const uint8_t r;
+    static const uint8_t g;
+    static const uint8_t b;
+};
 
+struct GreenBrick {
+    static const double x_dim_;
+    static const double y_dim_;
+    static const double z_dim_;
+    static const uint8_t r;
+    static const uint8_t g;
+    static const uint8_t b;
+};
+
+struct BlueBrick {
+    static const double x_dim_;
+    static const double y_dim_;
+    static const double z_dim_;
+    static const uint8_t r;
+    static const uint8_t g;
+    static const uint8_t b;
+};
+
+struct OrangeBrick {
+    static const double x_dim_;
+    static const double y_dim_;
+    static const double z_dim_;
+    static const uint8_t r;
+    static const uint8_t g;
+    static const uint8_t b;
 };
 
 #endif //BRICK_H
