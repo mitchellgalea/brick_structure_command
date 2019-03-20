8468a6acbf9f74e8ca92b483a1ee5ca0c989f6ec
mitchellgalea <48578916+mitchellgalea@users.noreply.github.com>
2019-03-20 21:52:19 +1100

    Update brick.cpp

--- src/brick.cpp
+++ src/brick.cpp
@@ -1,31 +1,38 @@
 #include "brick_structure_command/brick.h"
 
+Brick::Brick() {}
 
-/*********************************************************************
-* Comment
-*********************************************************************/
-Brick::Brick()
-{
+Brick::getXDim() const { return BrickType::x_dim; }
+Brick::getYDim() const { return BrickType::y_dim; }
+Brick::getZDim() const { return BrickType::z_dim; }
+Brick::getR() const { return BrickType::r; }
+Brick::getG() const { return BrickType::g; }
+Brick::getB() const { return BrickType::b; }
 
-}
-/*********************************************************************
-* Comment
-*********************************************************************/
-double Brick::getX() const
-{
-    return x_;
-}
-/*********************************************************************
-* Comment
-*********************************************************************/
-double Brick::getY() const
-{
-    return y_;
-}
-/*********************************************************************
-* Comment
-*********************************************************************/
-double Brick::getZ() const
-{
-    return z_;
-}
+constexpr double RedBrick::x_dim = 0.3;
+constexpr double RedBrick::y_dim = 0.2;
+constexpr double RedBrick::z_dim = 0.2;
+const uint8_t RedBrick::r = 255;
+const uint8_t RedBrick::g = 0;
+const uint8_t RedBrick::b = 0;
+
+constexpr double GreenBrick::x_dim = 0.6;
+constexpr double GreenBrick::y_dim = 0.2;
+constexpr double GreenBrick::z_dim = 0.2;
+const uint8_t GreenBrick::r = 0;
+const uint8_t GreenBrick::g = 255;
+const uint8_t GreenBrick::b = 0;
+
+constexpr double BlueBrick::x_dim = 1.2;
+constexpr double BlueBrick::y_dim = 0.2;
+constexpr double BlueBrick::z_dim = 0.2;
+const uint8_t BlueBrick::r = 0;
+const uint8_t BlueBrick::g = 0;
+const uint8_t BlueBrick::b = 255;
+
+constexpr double OrangeBrick::x_dim = 1.8;
+constexpr double OrangeBrick::y_dim = 0.2;
+constexpr double OrangeBrick::z_dim = 0.2;
+const uint8_t OrangeBrick::r = ;
+const uint8_t OrangeBrick::g = 255;
+const uint8_t OrangeBrick::b = 0;
