16a9472abdbe8ac9050e297e612cf02f6d83d40f
mitchellgalea <48578916+mitchellgalea@users.noreply.github.com>
2019-03-21 08:13:08 +1100

    Update brick_structure.h

--- include/brick_structure_command/brick_structure.h
+++ include/brick_structure_command/brick_structure.h
@@ -1,8 +1,34 @@
 #ifndef BRICKSTRUCTURE_H
 #define BRICKSTRUCTURE_H
 
-class BrickStructure {
+#include <vector>
+#include <memory>
 
+#include "brick_structure_command/brick.h"
+#include "brick_structure_command/BrickCommand.h"
+
+class BrickStructure {
+private:
+    //////// PRIVATE MEMBERS
+    std::vector<shared_ptr<B>> bricks_;
+    unsigned c_brick_count_;
+    
+    //////// PRIVATE METHODS
+    bool checkBrickHeights();
+    bool checkConstruct();
+    bool checkPosition();
+public:
+    //////// CONSTRUCTORS
+    BrickStructure();
+    
+    //////// GETTERS
+    unsigned getCBrickCount() const;
+    
+    //////// METHODS
+    void incCBrickCount();
+    BrickCommand brickCommand();
+    
+    
 };
 
 
